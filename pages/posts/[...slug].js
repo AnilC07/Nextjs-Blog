08668e8f7f4dada6e5141c39e0d8db2033c452eb
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-01 22:02:57 +0200

    create slug page to display specific post

---
+++ pages/posts/[...slug].js
@@ -0,0 +1,7 @@
+import PostContent from '../../components/posts/post-detail/post-content'
+// On utilise le slug pour que ce soit 'human readable' et 'search engine friendly'
+const PostDetailPage = (props) => {
+  return <PostContent />;
+};
+
+export default PostDetailPage;
